4802f039d0be0a30bffb5c2f285d242ac5bfb067
austin <doublehoon2@gmail.com>
2026-03-15 16:45:59 +0900

    store

--- store-service/src/main/java/com/delivery/store/controller/StoreController.java
+++ store-service/src/main/java/com/delivery/store/controller/StoreController.java
@@ -1,14 +1,50 @@
-package com.delivery.store;
+package com.delivery.store.controller;
 
 import com.delivery.common.ApiResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.delivery.store.dto.*;
+import com.delivery.store.service.StoreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/stores")
+@RequiredArgsConstructor
 public class StoreController {
 
-    @GetMapping("/api/stores/hello")
-    public ApiResponse<String> hello() {
-        return new ApiResponse<>(true, "store-service", "ok");
+    private final StoreService storeService;
+
+    @PostMapping
+    public ApiResponse<StoreResponse> createStore(
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestHeader("X-User-Email") String email,
+            @RequestHeader("X-User-Role") String role,
+            @Valid @RequestBody CreateStoreRequest request
+            ) {
+        return storeService.createStore(userId, email, role, request);
+    }
+    @GetMapping
+    public ApiResponse<List<StoreListResponse>> getStores() {
+        return storeService.getStores();
+    }
+
+    @PostMapping("/{storeId}")
+    public ApiResponse<StoreDetailResponse> getStoreDetail(@PathVariable Long storeId) {
+        return storeService.getStoreDetail(storeId);
+    }
+
+    @PostMapping("/{storeId}/menus")
+    public ApiResponse<MenuResponse> createMenu(@PathVariable Long storeId,
+                                                @RequestHeader("X-User-Id") Long userId,
+                                                @RequestHeader("X-User-Role") String role,
+                                                @Valid @RequestBody CreateMenuRequest request) {
+        return storeService.createMenu(storeId, userId, role, request);
+    }
+
+    @GetMapping("/{storeId}/menus")
+    public ApiResponse<List<MenuResponse>> getMenus(@PathVariable Long storeId) {
+        return storeService.getMenus(storeId);
     }
 }
