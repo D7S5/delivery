4675dafeef29f7c40eff1125efcbdbf8493972b4
austin <doublehoon2@gmail.com>
2026-03-26 14:31:16 +0900

    controller

--- order-service/src/main/java/com/delivery/order/controller/OrderInternalController.java
+++ order-service/src/main/java/com/delivery/order/controller/OrderInternalController.java
@@ -14,25 +14,21 @@ public class OrderInternalController {
 
     @PutMapping("/{orderId}/prepared")
     public ApiResponse<Void> prepared(@PathVariable Long orderId) {
-        System.out.println("orderId = " + orderId);
         return service.prepared(orderId);
     }
 
     @PutMapping("/{orderId}/delivery")
     public ApiResponse<Void> delivery(@PathVariable Long orderId) {
-        System.out.println("orderId = " + orderId);
         return service.delivery(orderId);
     }
 
     @PutMapping("/{orderId}/complete")
     public ApiResponse<Void> orderComplete(@PathVariable Long orderId) {
-        System.out.println("orderId = " + orderId);
         return service.complete(orderId);
     }
 
     @PutMapping("/{orderId}/cancel")
     public ApiResponse<Void> orderCancel(@PathVariable Long orderId) {
-        System.out.println("orderId = " + orderId);
         return service.cancel(orderId);
     }
 }
