945cd40ffc536997189d9a82fc46cf3a46d77502
austin <doublehoon2@gmail.com>
2026-03-29 16:11:58 +0900

    online

--- rider-service/src/main/java/com/example/riderservice/controller/RiderController.java
+++ rider-service/src/main/java/com/example/riderservice/controller/RiderController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/riders")
+@RequestMapping("/api/rider")
 @RequiredArgsConstructor
 public class RiderController {
 
