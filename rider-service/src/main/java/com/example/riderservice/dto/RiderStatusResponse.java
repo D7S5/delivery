6521073743ed88925f55c5b42af287838e6f9f41
austin <doublehoon2@gmail.com>
2026-03-29 16:28:00 +0900

    online

--- rider-service/src/main/java/com/example/riderservice/dto/RiderStatusResponse.java
+++ rider-service/src/main/java/com/example/riderservice/dto/RiderStatusResponse.java
@@ -5,7 +5,7 @@ import com.example.riderservice.entity.RiderStatus;
 public record RiderStatusResponse(
         Long riderId,
         Long userId,
-        RiderStatus Status,
+        RiderStatus status,
         boolean online
 ) {
 }
